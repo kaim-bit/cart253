038944df5fd4aa510e38676af10a024455505989
kaim-bit <kmaquivar@outlook.com>
2025-09-21 12:46:06 -0400

    Made the spine and part of the ribcage

--- topics/art-jam/template-p5-project/js/script.js
+++ topics/art-jam/template-p5-project/js/script.js
@@ -13,11 +13,40 @@
 function setup() {
     createCanvas(600,600)
 }
+// draws the backround color
+function backround_draw()
+{
+    background(100,0,0)
+}
+
+//secret skeleton draw
+function secret_skeleton()
+{
+    //spine
+    push()
+    noStroke()
+    fill(255,255,255)
+    rect(300,200,20,400)
+
+    
+    fill(255,255,255)
+    rect(210,250,200,20)
+
+    fill(255,255,255)
+    rect(210,300,200,20)
+
+    fill(255,255,255)
+    rect(210,350,200,20)
+
+    
 
 
+}
 /**
  * every frame creating and coloring shapes
 */
-function draw() {
- backround(0,0,2)
+function draw() 
+{
+ backround_draw()
+ secret_skeleton()
 }
