c1bb084be3cce296df400bcd09f2d3040d973781
kaim-bit <kmaquivar@outlook.com>
2025-10-28 14:50:26 -0400

    Finished Array Challenge

---
+++ topics/Arrays-Challenge/template-p5-project/js/script.js
@@ -0,0 +1,103 @@
+/**
+ * Boingo
+ * Pippin Barr
+ * edited by kai, benedict
+ * A ball that bounces around on the canvas
+ */
+
+
+
+/**
+ * Create the canvas and the ball
+ */
+
+const ballS = [];
+function setup() {
+    // Create the canvas
+    createCanvas(400, 400);
+    // Create the ball
+    for (let r = 0; r < 10; r++) {
+        let ball = createBall();
+        ballS.push(ball);
+    }
+
+}
+
+/**
+ * Creates a random ball
+ */
+function createBall() {
+    // Create a ball object with appropriate properties
+    const newBall = {
+        // Position and dimensions
+        x: 200,
+        y: 200,
+        size: 20,
+        // Colour
+        fill: "#000000",
+        // Movement
+        velocity: {
+            x: random(-5, 5),
+            y: random(-5, 5)
+        }
+    };
+    return newBall;
+}
+
+/**
+ * Moves and draws the ball
+ */
+function draw() {
+    background("#87ceeb");
+    for (let i = 0; i < ballS.length; i++) {
+        moveBall(ballS[i]);
+        bounceBall(ballS[i]);
+        drawBall(ballS[i]);
+    }
+}
+
+/**
+ * Moves the ball according to its velocity
+ */
+function moveBall(ball) {
+    ball.x += ball.velocity.x;
+    ball.y += ball.velocity.y;
+}
+
+/**
+ * Bounces the ball off the walls
+ */
+function bounceBall(ball) {
+    // Check if the ball has reached the left or right
+    const bounceX = (ball.x > width || ball.x < 0);
+    // Check if the ball has reached the top or bottom
+    const bounceY = (ball.y > height || ball.y < 0);
+
+    // Handle bouncing horizontally
+    if (bounceX) {
+        ball.velocity.x *= -1;
+    }
+    // Handle bouncing vertically
+    if (bounceY) {
+        ball.velocity.y *= -1;
+    }
+}
+
+/**
+ * Draw the ball on the canvas
+ */
+function drawBall(ball) {
+    push();
+    noStroke();
+    fill(ball.fill);
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
+}
+
+function mousePressed() {
+    for (let i = 0; i < 100; i++) {
+        let ball = createBall();
+        ballS.push(ball);
+    }
+
+}
